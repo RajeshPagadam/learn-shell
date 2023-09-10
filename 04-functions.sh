ec46a77b173fcbb0e498e12ee51f24c3c5db95fc
Rajesh <rajesh.raju370@gmail.com>
2023-09-10 12:48:07 +0530

    echo command

---
+++ 04-functions.sh
@@ -0,0 +1,12 @@
+# If we assign a name to commands , then it is a function
+
+# Declare a function
+abc() {
+  echo Hello World
+  echo Hello Universe
+  echo Hello Galaxy
+}
+
+# How to access a function
+abc
+
